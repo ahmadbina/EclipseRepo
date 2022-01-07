ad03557ce4f51eba115183be3264b69a484284bb
ahmadbina <97071721+ahmadbina@users.noreply.github.com>
2022-01-07 16:28:09 -0500

    Update FirstClass.java

--- GitEclipseProject/src/GitEclipse/FirstClass.java
+++ GitEclipseProject/src/GitEclipse/FirstClass.java
@@ -3,8 +3,27 @@ package GitEclipse;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String original = "Tamim";
+		String reverse ="";
+		for (int i = original.length()-1; i >=0 ; i--) {
+			
+			reverse= reverse+ original.charAt(i);
+			
+		}
+		
+		if ( original.equals(reverse)) {
+			
+			System.out.println("\nIt is a Palindrome");
+		}else {
+			System.out.println("\nIt is not a Palindrome");
+		}
 
+		String sh = "tamim";
+		
+		StringBuilder sb = new StringBuilder(sh);
+		
+		sb.reverse();
+		System.out.println(String.valueOf(sb).equals(sh));
 	}
 
 }
