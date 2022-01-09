b141eff4d34bf8361fa5da4a398462e868ebf2f9
ahmadbina <binaahmad616@gmail.com>
2022-01-08 21:31:32 -0500

    changed

--- GitEclipseProject/src/GitEclipse/ScannerEX.java
+++ GitEclipseProject/src/GitEclipse/ScannerEX.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class ScannerEX {
 
 	public static void main(String[] args) {
-		String Boss = "Tamim";
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter our name");
 		String name=scanner.nextLine();
+		String Boss = "Tamim";
+		Boss +=name;
 		if(Boss=="Tamim") {
 			System.out.println("The right name");
 		}else {
